1890d9dc746083646a2a60ed7e04f7f65d1c0f35
Josh Webb <joshwebb42@gmail.com>
2019-12-11 04:53:47 -0600

    minimal styling

--- inc/header.php
+++ inc/header.php
@@ -4,13 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <title><?php echo $pageTitle; ?></title>
 </head>
 <body>
 <header>
-<ul class="nav">
+<nav class="navbar navbar-dark bg-dark">
+<a class="navbar-brand" href="index.php">
+    <img src="img/vinylRecordLogo.png" width="50" height="50" alt="logo">
+  </a>
   <li class="nav-item">
     <a class="nav-link active" href="catalog.php?cat=regular">"Real" Music</a>
   </li>
@@ -20,6 +24,6 @@
   <li class="nav-item">
     <a class="nav-link" href="catalog.php?cat=cover">Covers</a>
   </li>
-</ul>
+</nav>
 </header>
 <div class="content">
